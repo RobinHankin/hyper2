72fb74c0e81d29af065690b22ec2dcbccb87d935
RobinHankin <hankin.robin@gmail.com>
2017-10-02 08:55:00 +1300

    kickoff!

---
+++ data/chess.R
@@ -0,0 +1,5 @@
+`chess` <- 
+structure(list(brackets = list(1, c(1, 2), c(1, 3), 2, c(2, 3
+), 3), powers = c(30, -35, -18, 36, -34, 22), pnames = c("Topalov", 
+"Anand", "Karpov")), .Names = c("brackets", "powers", "pnames"
+), class = "hyper2")
---
+++ inst/rowing_analysis.R
@@ -0,0 +1,21 @@
+library(hyper2)
+rm(list=ls())
+
+filename <- "rowing.txt"  # could be rowing_minimal.txt
+o <- strsplit(readLines(filename)," ")
+
+rowers <- sort(unique(unlist(o)))
+H <- hyper2(list(),0,pnames=rowers)
+
+for(v in o){
+    v <- rev(v)
+    for(i in seq_along(v)){
+        H[v[i]] <- powers(H[v[i]]) + 1
+        H[v[seq_len(i)]] <- powers(H[v[seq_len(i)]])-1
+    }
+}
+
+I <- hyper2(pnames=rowers)
+for(v in o){
+  I <- I+order_likelihood(character_to_number(v,rowers))
+}
