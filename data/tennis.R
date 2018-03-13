4fbefb47670ea9aad23dc13012bbb77f8b8b5241
RobinHankin <hankin.robin@gmail.com>
2018-03-14 11:11:56 +1300

    add a formal data object

---
+++ data/tennis.R
@@ -0,0 +1,15 @@
+`tennis` <-
+  structure(list(brackets = list(1, c(1, 2), c(1, 2, 3, 4), c(1, 
+3), c(1, 4), 2, c(2, 3), c(2, 4), 3, c(3, 4), 4), powers = c(20, 
+9, -32, -20, -18, 23, -19, -17, 41, 2, 37), pnames = c("P1", 
+"P2", "P3", "P4")), .Names = c("brackets", "powers", "pnames"
+), class = "hyper2")
+
+`tennis_ghost` <- 
+structure(list(brackets = list(1, c(1, 2, 3, 4), c(1, 2, 3, 4, 
+5), c(1, 2, 5), c(1, 3), c(1, 4), 2, c(2, 3), c(2, 4), 3, c(3, 
+4), 4), powers = c(20, -21, -11, 9, -20, -18, 23, -19, -17, 41, 
+-24, 37), pnames = c("P1", "P2", "P3", "P4", "ghost")), .Names = c("brackets", 
+"powers", "pnames"), class = "hyper2")
+
+
