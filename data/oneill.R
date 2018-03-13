b2138add9b31613a0131200d54babbd683282836
RobinHankin <hankin.robin@gmail.com>
2018-03-14 11:32:54 +1300

    add the climate dataset from Saffron

---
+++ data/oneill.R
@@ -0,0 +1,7 @@
+`icons` <-
+structure(list(brackets = list(1, c(1, 2, 4, 5), c(1, 2, 4, 6
+), c(1, 2, 5, 6), c(1, 3, 4, 5), c(1, 3, 4, 6), c(1, 3, 5, 6), 
+    2, c(2, 3, 4, 5), c(2, 3, 4, 6), c(2, 3, 5, 6), 3, 4, 5, 
+    6), powers = c(32, -20, -15, -9, -18, -18, -8, 24, -11, -16, 
+-18, 30, 24, 14, 9), pnames = c("NB", "L", "PB", "THC", "OA", 
+"WAIS")), .Names = c("brackets", "powers", "pnames"), class = "hyper2")
