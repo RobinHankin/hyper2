b028cc45b97bd0bdd81525b52e1340299b414d1b
RobinHankin <hankin.robin@gmail.com>
2018-05-14 14:18:51 +1200

    new formula 1 results

---
+++ inst/formula1.R
@@ -0,0 +1,77 @@
+library("hyper2")
+library("magrittr")
+
+
+## File formula1.txt is directly copied from Wikipedia (with slight whitespace changes)
+wiki_table <- read.table("formula1.txt",header=TRUE)
+racers <- wiki_table[,1]
+venues <- colnames(wiki_table)[-c(1,22)]
+
+## need to count any of Ret (retired), WD (withdrawn) or DNS (did not start) as a zero:
+noscore <- c("Ret", "WD", "DNS")
+f <- function(x){
+  if(any(x %in% noscore)){x[x%in%noscore] <- 0}
+  return(x)
+}
+
+numerical_table <- apply(wiki_table,2,f)
+
+
+# Now create a numeric matrix:
+fmat <- matrix(as.numeric(numerical_table[,-c(1,22)]),byrow=TRUE,ncol=nrow(wiki_table)) # convert to numeric and strip out names; transpose of wiki_table
+
+colnames(fmat) <- racers
+rownames(fmat) <- venues
+
+## Thus the first row of fmat is AUS (Australia), in which Hamilton camge second, Vettel came first, etc.
+## The first column of fmat is Hamilton's results. He came second in AUS, first in CHN, second in BHR, etc.
+
+
+## Following is similar to, but slightly different from, the analysis in eurovision.R:
+## Define an empty hyper2 object:
+F1 <- hyper2(d=ncol(fmat))
+
+for(i in seq_len(nrow(fmat))){   # cycle through the rows; each row is a voter
+    d <- fmat[i,,drop=TRUE]
+    d[is.na(d)] <- -1  # kludge: make the voting country ineligible to vote.
+    while(any(d>0)){
+      eligible <- which(d>=0)  
+
+      ## The first choice among eligible players has +1 power on the
+      ## numerator:
+      F1[which(d==1)] %<>% "+"(1)
+
+      ## denominator of all eligible players; power -1
+      F1[eligible] %<>% "-"(1)
+
+      ## once you've come first in the field, you are ineligible to be first again:
+      d[d==1] <- -1  
+      
+      ## everyone moves down the list, so who *was* in second place
+      ## becomes first place, who *was* third place becomes second,
+      ## and so on:
+      d[d>0] %<>% "-"(1)
+
+    } # while() loop closes
+} # i loop closes
+
+
+## syntatic sugar:
+pnames(F1) <- racers
+
+m <- maxp(F1)
+pdf(file="f.pdf")
+dotchart(m,pch=16)
+dev.off()
+
+pdf(file="g.pdf")
+
+ox <- order(wiki_table$points,decreasing=TRUE)
+oy <- order(m,decreasing=TRUE)
+par(pty='s') # square plot
+plot(ox,oy,asp=1,pty='s',xlim=c(0,25),ylim=c(0,25),pch=16,xlab="official order",ylab="my order")
+par(xpd=TRUE) # allow drivers' names to appear outside plotting region
+for(i in seq_along(ox)){  text(ox[i],oy[i],names(m)[ox[i]],pos=4,col='gray') }
+par(xpd=FALSE) # stop diagonal line from protruding beyond plotting region
+abline(0,1)
+dev.off()
