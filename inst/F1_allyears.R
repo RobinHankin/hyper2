5c4fac113bdf203bdc52a8178d22702913314942
RobinHankin <hankin.robin@gmail.com>
2018-05-17 14:52:19 +1200

    better example

--- inst/F1_allyears.R
+++ inst/F1_allyears.R
@@ -21,15 +21,14 @@ for(i in seq_along(files)){
 drivers %<>% unique %>% sort
 
 
-
 likelihood_from_finishing_order <- function(H, df){
-  ## 'H' a pre-existing hyper2 object, 'df' a dataframe such as
+  ## 'H' a pre-existing hyper2 object [needed because we have to know
+  ## the drivers' names ahead of time], 'df' a dataframe such as
   ## read.table("formula1_2017.txt",header=T)
-    LARGE <- 9999
 
     drivers <- as.character(df$driver)
 
-    for(i in seq(from=2,to=ncol(df)-1)){
+    for(i in seq(from=2,to=ncol(df)-1)){  # first column is driver, last is points
         d <- as.numeric(as.character(df[,i,drop=TRUE])) # coerces text to NA
         jj <- is.na(d)|(d==0)
         nonfinishers <- drivers[jj]
@@ -52,11 +51,13 @@ if(FALSE){  # too slow!
         H %<>% likelihood_from_finishing_order(p)
     }
 } else {  ## Much faster (identical result):
-    H1 <- likelihood_from_finishing_order(hyper2(pnames=drivers),allyears[[1]])
-    H2 <- likelihood_from_finishing_order(hyper2(pnames=drivers),allyears[[2]])
-    H3 <- likelihood_from_finishing_order(hyper2(pnames=drivers),allyears[[3]])
-    H4 <- likelihood_from_finishing_order(hyper2(pnames=drivers),allyears[[4]])
+  jj <- list(
+    H1 = likelihood_from_finishing_order(hyper2(pnames=drivers),allyears[[1]])
+    H2 = likelihood_from_finishing_order(hyper2(pnames=drivers),allyears[[2]])
+    H3 = likelihood_from_finishing_order(hyper2(pnames=drivers),allyears[[3]])
+    H4 = likelihood_from_finishing_order(hyper2(pnames=drivers),allyears[[4]])
     
-    HH <- H1+H2+H3+H4
+    ## HH = H1+H2+H3+H4
+    HH <- Reduce("+",jj)
 }
 
