2858105942f5485c2315ce11c957815a6e030bfa
RobinHankin <hankin.robin@gmail.com>
2019-02-04 20:09:08 +1300

    need to add the file

---
+++ data/handover.R
@@ -0,0 +1,4 @@
+`handover` <-
+structure(list(brackets = list(1, c(1, 2), c(1, 3), 2, c(2, 4
+), 3, c(3, 4), 4), powers = c(18, 4, 9, 1, 2, 5, 4, 26), pnames = c("t00", 
+"t10", "t01", "t11")), class = "hyper2")
