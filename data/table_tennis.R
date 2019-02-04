50e58cdc87514a88ef64046877f3aaa5ef1e5074
RobinHankin <hankin.robin@gmail.com>
2019-02-04 20:53:07 +1300

    include the table tennis dataset (importance of serve)

---
+++ data/table_tennis.R
@@ -0,0 +1,4 @@
+`table_tennis` <-
+structure(list(brackets = list(c(1, 2), c(1, 2, 3), c(1, 2, 4
+), c(1, 3), c(1, 4), 2, 3, 4), powers = c(9, -10, -8, 3, 2, 2, 
+1, 1), pnames = c("S", "a", "b", "c")), class = "hyper2")
