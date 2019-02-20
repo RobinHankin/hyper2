b11b8b84903e8f8494aacab08ff88bae5b0b77ab
RobinHankin <hankin.robin@gmail.com>
2019-02-20 14:05:59 +1300

    correction to dataset, final fix to issue #12

--- data/karpov_kasparov_anand.R
+++ data/karpov_kasparov_anand.R
@@ -28,6 +28,5 @@ karpov_draws_anand = 28
 `karpov_kasparov_anand` <-
 structure(list(brackets = list(1, c(1, 2, 4, 5), c(1, 3, 4, 5
 ), c(1, 4), 2, c(2, 3, 4, 5), c(2, 4), 3, c(3, 4), 5), powers = c(22, 
--187, -71, 25, 9, -63, 45, 16, 24, 180), pnames = c("Karpov", 
+-185, -71, 25, 9, -65, 45, 16, 24, 180), pnames = c("Karpov", 
 "Kasparov", "Anand", "white", "draw")), class = "hyper2")
-
