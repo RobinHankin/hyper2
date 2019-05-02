73d0f3cbd33eb68cf3a74b273e9bb62e6c3aa442
RobinHankin <hankin.robin@gmail.com>
2019-05-03 08:21:56 +1200

    further work on the kka dataset

--- data/karpov_kasparov_anand.R
+++ data/karpov_kasparov_anand.R
@@ -4,6 +4,11 @@
 ## calculated in files inst/karpov_kasparov_anand.R and
 ## inst/kka_3draws.R respectively.
 
+## The kka dataset is also used, in inst/kka_array.R, to calculate
+## tables such as `white_wins` and `kka_array`.
+
+## Help on these objects is given in man/karpov_kasparov_anand.Rd.
+
 kka <- c(
 ## Kasparov vs Karpov
 karpov_plays_white_beats_kasparov = 18,  # 12 on p1, 6 on p2
@@ -31,17 +36,49 @@ anand_plays_white_draws_karpov = 20
 )
 
 
-
-
+# Likelihood functions calculated in inst/kka_3draws.R follows:
 `karpov_kasparov_anand` <-
 structure(list(brackets = list(1, c(1, 2, 4, 5), c(1, 3, 4, 5
 ), c(1, 4), 2, c(2, 3, 4, 5), c(2, 4), 3, c(3, 4), 5), powers = c(12, 
 -193, -92, 25, 20, -80, 45, 15, 24, 224), pnames = c("Karpov", 
 "Kasparov", "Anand", "white", "draw")), class = "hyper2")
 
+# Likelihood functions calculated in inst/karpov_kasparov_anand.R follows:
 `kka_3draws` <-
-  structure(list(brackets = list(1, c(1, 2, 4, 5, 6), c(1, 3, 4, 
+structure(list(brackets = list(1, c(1, 2, 4, 5, 6), c(1, 3, 4, 
 5, 7), c(1, 4), 2, c(2, 3, 4, 6, 7), c(2, 4), 3, c(3, 4), c(5, 
 6), c(5, 7), c(6, 7)), powers = c(12, -193, -92, 25, 20, -80, 
 45, 15, 24, 129, 49, 46), pnames = c("Karpov", "Kasparov", "Anand", 
 "white", "Karpov_draw", "Kasparov_draw", "Anand_draw")), class = "hyper2")
+
+
+## Matrices of scorelines calculated in ist/kka_array.R follows:
+`white_wins` <-
+  structure(c(NA, 7, 15, 18, NA, 30, 6, 18, NA),
+            .Dim = c(3L, 3L),
+            .Dimnames = list(
+                plays_white_wins = c("Anand", "Karpov", "Kasparov"),
+                plays_black_loses = c("Anand", "Karpov", "Kasparov")))
+
+`drawn_games` <-
+  structure(c(NA, 29, 26, 20, NA, 57, 20, 72, NA),
+            .Dim = c(3L, 3L),
+            .Dimnames = list(
+                plays_white_draws = c("Anand", "Karpov", "Kasparov"),
+                plays_black_draws = c("Anand", "Karpov", "Kasparov")))
+
+`black_wins` <-
+  structure(c(NA, 5, 11, 13, NA, 9, 2, 7, NA),
+            .Dim = c(3L, 3L),
+            .Dimnames = list(
+                plays_black_wins = c("Anand", "Karpov", "Kasparov"),
+                plays_black_loses = c("Anand", "Karpov", "Kasparov")))
+
+`kka_array` <-
+structure(c(NA, 7, 15, 18, NA, 30, 6, 18, NA, NA, 29, 26, 20, 
+            NA, 57, 20, 72, NA, NA, 5, 11, 13, NA, 9, 2, 7, NA),
+          .Dim = c(3L, 3L, 3L),
+          .Dimnames = list(
+              c("Anand", "Karpov", "Kasparov"),
+              c("Anand", "Karpov", "Kasparov"),
+              c("white_wins","drawn_games","black_wins")))
--- inst/kka_array.R
+++ inst/kka_array.R
@@ -2,31 +2,38 @@
 ## karpov/kasparov/anand dataset.
 
 library("hyper2")
+library("abind")
 attach(as.list(kka))
         
 players <- c("Anand","Karpov","Kasparov")
 
-jj_white_wins <- matrix(NA,3,3)
-dimnames(jj_white_wins) <- list(plays_white_wins=players,plays_black_loses=players)
-jj_white_wins["Anand"   ,"Karpov"  ] <- anand_plays_white_beats_karpov
-jj_white_wins["Anand"   ,"Kasparov"] <- anand_plays_white_beats_kasparov
-jj_white_wins["Karpov"  ,"Anand"   ] <- karpov_plays_white_beats_anand
-jj_white_wins["Karpov"  ,"Kasparov"] <- karpov_plays_white_beats_kasparov
-jj_white_wins["Kasparov","Anand"   ] <- kasparov_plays_white_beats_anand
-jj_white_wins["Kasparov","Karpov"  ] <- kasparov_plays_white_beats_karpov
+white_wins <- matrix(NA,3,3)
+dimnames(white_wins) <- list(plays_white_wins=players,plays_black_loses=players)
+white_wins["Anand"   ,"Karpov"  ] <- anand_plays_white_beats_karpov
+white_wins["Anand"   ,"Kasparov"] <- anand_plays_white_beats_kasparov
+white_wins["Karpov"  ,"Anand"   ] <- karpov_plays_white_beats_anand
+white_wins["Karpov"  ,"Kasparov"] <- karpov_plays_white_beats_kasparov
+white_wins["Kasparov","Anand"   ] <- kasparov_plays_white_beats_anand
+white_wins["Kasparov","Karpov"  ] <- kasparov_plays_white_beats_karpov
+
+drawn_games <- matrix(NA,3,3)
+dimnames(drawn_games) <- list(plays_white_draws=players,plays_black_draws=players)
+drawn_games["Anand"   ,"Karpov"  ] <- anand_plays_white_draws_karpov
+drawn_games["Anand"   ,"Kasparov"] <- anand_plays_white_draws_kasparov
+drawn_games["Karpov"  ,"Anand"   ] <- karpov_plays_white_draws_anand
+drawn_games["Karpov"  ,"Kasparov"] <- karpov_plays_white_draws_kasparov
+drawn_games["Kasparov","Anand"   ] <- kasparov_plays_white_draws_anand
+drawn_games["Kasparov","Karpov"  ] <- kasparov_plays_white_draws_karpov
 
 
-jj_draw <- matrix(NA,3,3)
-dimnames(jj_draw) <- list(plays_white_draws=players,plays_black_draws=players)
+black_wins <- matrix(NA,3,3)
+dimnames(black_wins) <- list(plays_black_wins=players,plays_black_loses=players)
+black_wins["Anand"   ,"Karpov"  ] <- anand_plays_black_beats_karpov
+black_wins["Anand"   ,"Kasparov"] <- anand_plays_black_beats_kasparov
+black_wins["Karpov"  ,"Anand"   ] <- karpov_plays_black_beats_anand
+black_wins["Karpov"  ,"Kasparov"] <- karpov_plays_black_beats_kasparov
+black_wins["Kasparov","Anand"   ] <- kasparov_plays_black_beats_anand
+black_wins["Kasparov","Karpov"  ] <- kasparov_plays_black_beats_karpov
 
-jj_black_wins <- matrix(NA,3,3)
-dimnames(jj_black_wins) <- list(plays_black_wins=players,plays_black_loses=players)
-
-out <- abind(
-    white_wins=jj_white_wins,
-    draw=jj_draw,
-    black_wins=jj_black_wins,
-    along=3)
-
-
-
+kka_array <- abind(white_wins,drawn_games,black_wins,along=3)
+detach(as.list(kka))
