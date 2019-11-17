01b2525730947785dc0f9d7107a98c2b8d7c4e2b
RobinHankin <hankin.robin@gmail.com>
2019-11-18 08:23:08 +1300

    maxplist() now part of the package following 080e32c

--- inst/losers.R
+++ inst/losers.R
@@ -1,4 +1,4 @@
-## Function losers() gives a likelihood function for situations where
+## Function elimination() gives a likelihood function for situations where
 ## the _weakest_ player is eliminated at each stage; it returns a
 ## list.  It is intended for situations like the Great British
 ## Bake-off and Masterchef.  Function maxp2(), which is very similar
@@ -6,7 +6,7 @@
 ## object.
 
 
-`losers` <- function(players){
+`elimination` <- function(players){
     H <- choose_losers(hyper2(pnames=players),players,players[length(players)])
     players <- players[-length(players)]
     while(length(players)>1){
@@ -20,26 +20,9 @@
 }
 
 
-maxp2 <- function (H, startp = NULL, give = FALSE, fcm = NULL, fcv = NULL, ...){
-    SMALL <- 1e-06
-    n <- size(H[[1]])
-    if (is.null(startp)) {
-        startp <- rep(1/n, n - 1)
-    }
-    objective <- function(p) {
-        -like_single_list(p, H)
-    }
-    
-    UI <- rbind(diag(nrow = n - 1), -1, fcm)
-    CI <- c(rep(SMALL, n - 1), -1 + SMALL, fcv)
-    out <- constrOptim(theta = startp, f = objective, grad = NULL, 
-        ui = UI, ci = CI, ...)
-    out$value <- -out$value
-    if (give) {
-        return(out)
-    }
-    else {
-        jj <- fillup(out$par)
-        return(jj)
-    }
-}
+
+
+o <- maxplist(elimination(players=1:4))  # 12 terms
+dotchart(o,pch=16)
+
+
