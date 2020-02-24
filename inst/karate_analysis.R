591cfc0f6d6f711ef77bbad8a66c4221ac06a5a5
RobinHankin <hankin.robin@gmail.com>
2020-02-24 16:42:02 +1300

    fixes #33

--- inst/karate_analysis.R
+++ inst/karate_analysis.R
@@ -14,22 +14,28 @@ library("magrittr")
 a <- read.table("karate.txt",colClasses = "character",header=TRUE)
 a$wins1 <-  as.numeric(a$wins1)
 a$wins2 <-  as.numeric(a$wins2)
-attach(a)
 
-H <- hyper2(pnames=sort(unique(c(karateka1,karateka2))))
+H <- hyper2(pnames=sort(unique(c(a$karateka1,a$karateka2))))
 for(i in seq_len(nrow(a))){
 
-  H[karateka1] %<>% inc(wins1)
-  H[karateka2] %<>% inc(wins2)
-  H[c(karateka1,karateka2)] %<>% dec(wins1+wins2)
+  p1 <- a$karateka1[i]
+  p2 <- a$karateka2[i]
+
+  w1 <- a$wins1[i]
+  w2 <- a$wins2[i]
+
+  
+  H[p1] %<>% inc(w1)
+  H[p2] %<>% inc(w2)
+  H[c(p1,p2)] %<>% dec(w1+w2)
 }
 
-detach(a)
+
 Hcut <- H
 for(i in 1:30){
   print(paste("iteration ",i,", size(H) = ",size(Hcut),sep=""))
   m <- maxp(Hcut)
-  too_small <- (m < 1e-4)
+  too_small <- (m < 1e-3)
   if(any(too_small)){
     Hcut %<>% discard(names(m[too_small]))
   } else {
