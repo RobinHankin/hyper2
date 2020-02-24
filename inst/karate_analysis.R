9dcb406fc0fa830ef5fc815c82e03d340056fffa
RobinHankin <hankin.robin@gmail.com>
2020-02-24 16:21:05 +1300

    better and nicer text file, also more analysis

--- inst/karate_analysis.R
+++ inst/karate_analysis.R
@@ -5,29 +5,34 @@
 ## Encyclopedia. Retrieved 00:21, February 24, 2020, from
 ## https://en.wikipedia.org/w/index.php?title=2018_World_Karate_Championships_%E2%80%93_Men%27s_67_kg&oldid=938728758
 
+
+
+set.seed(0)
 library("hyper2")
 library("magrittr")
 
-a <- read.table("karate.txt",colClasses = "character")
+a <- read.table("karate.txt",colClasses = "character",header=TRUE)
+a$wins1 <-  as.numeric(a$wins1)
+a$wins2 <-  as.numeric(a$wins2)
+attach(a)
 
-
-comp1 <- a$V1
-wins1 <- as.numeric(a$V2)
-
-comp2 <- a$V3
-wins2 <- as.numeric(a$V4)
-
-
-H <- hyper2(pnames=sort(unique(c(comp1,comp2))))
+H <- hyper2(pnames=sort(unique(c(karateka1,karateka2))))
 for(i in seq_len(nrow(a))){
 
-  comp1 <- a$V1[i]
-  comp2 <- a$V3[i]
-  wins1 <- as.numeric(a$V2)[i]
-  wins2 <- as.numeric(a$V4)[i]
-  
-  H[comp1] %<>% inc(wins1)
-  H[comp2] %<>% inc(wins2)
-  H[c(comp1,comp2)] %<>% dec(wins1+wins2)
+  H[karateka1] %<>% inc(wins1)
+  H[karateka2] %<>% inc(wins2)
+  H[c(karateka1,karateka2)] %<>% dec(wins1+wins2)
 }
 
+detach(a)
+Hcut <- H
+for(i in 1:30){
+  print(paste("iteration ",i,", size(H) = ",size(Hcut),sep=""))
+  m <- maxp(Hcut)
+  too_small <- (m < 1e-4)
+  if(any(too_small)){
+    Hcut %<>% discard(names(m[too_small]))
+  } else {
+    break
+  }
+}
