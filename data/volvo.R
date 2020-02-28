f695affab151cec154be896e756f3b3393dae7bc
RobinHankin <hankin.robin@gmail.com>
2020-02-29 11:46:14 +1300

    Volvo ocean racing

---
+++ data/volvo.R
@@ -0,0 +1,29 @@
+`volvo2014` <- 
+structure(list(brackets = list(1, c(1, 2, 3, 4, 5), c(1, 2, 3, 
+4, 5, 6, 7), c(1, 2, 3, 4, 5, 7), c(1, 2, 3, 5, 6, 7), c(1, 2, 
+4, 5, 6, 7), c(1, 3, 4, 5, 6, 7), c(1, 3, 5, 6, 7), c(1, 3, 6, 
+7), c(1, 4, 5, 6, 7), c(1, 6, 7), 2, c(2, 3, 4, 5), c(2, 3, 4, 
+5, 6, 7), c(2, 3, 5), c(2, 3, 5, 6, 7), c(2, 3, 6, 7), c(2, 4, 
+5, 6, 7), c(2, 4, 6, 7), c(2, 5, 6, 7), c(2, 6, 7), 3, c(3, 5
+), c(3, 6, 7), 4, c(4, 5, 6), c(4, 5, 6, 7), c(4, 6), 5, c(5, 
+6, 7), 6, c(6, 7), 7), powers = c(8, -1, -8, -1, -1, -2, -2, 
+-1, -1, -1, -1, 8, -1, -2, -1, -2, -1, -3, -1, -1, -2, 6, -1, 
+-1, 7, -1, -3, -1, 8, -2, 8, -5, 2), pnames = c("AbuDhabi", "Brunel", 
+"Dongfeng", "MAPFRE", "Alvimedica", "SCA", "Vestas")), class = "hyper2")
+
+`volvo_table_2014` <-
+structure(list(leg1 = c(1L, 3L, 2L, 7L, 5L, 6L, 4L), leg2 = structure(c(3L, 
+1L, 2L, 4L, 5L, 6L, 7L), .Label = c("1", "2", "3", "4", "5", 
+"6", "DNF"), class = "factor"), leg3 = structure(c(2L, 5L, 1L, 
+4L, 3L, 6L, 7L), .Label = c("1", "2", "3", "4", "5", "6", "DNS"
+), class = "factor"), leg4 = structure(c(2L, 5L, 3L, 1L, 4L, 
+6L, 7L), .Label = c("1", "2", "3", "4", "5", "6", "DNS"), class = "factor"), 
+    leg5 = structure(c(1L, 4L, 6L, 2L, 3L, 5L, 7L), .Label = c("1", 
+    "2", "3", "4", "5", "DNF", "DNS"), class = "factor"), leg6 = structure(c(2L, 
+    3L, 1L, 4L, 5L, 6L, 7L), .Label = c("1", "2", "3", "4", "5", 
+    "6", "DNS"), class = "factor"), leg7 = structure(c(5L, 1L, 
+    4L, 2L, 3L, 6L, 7L), .Label = c("1", "2", "3", "4", "5", 
+    "6", "DNS"), class = "factor"), leg8 = c(3L, 5L, 7L, 4L, 
+    6L, 1L, 2L), leg9 = c(5L, 2L, 4L, 3L, 1L, 7L, 6L)), class = "data.frame", row.names = c("AbuDhabi", 
+"Brunel", "Dongfeng", "MAPFRE", "Alvimedica", "SCA", "Vestas"
+))
