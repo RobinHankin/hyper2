f52e1424e0e50f3bf454be3900c4b3da4753ece0
RobinHankin <hankin.robin@gmail.com>
2020-03-05 15:42:15 +1300

    pentathlon dataset

---
+++ data/pentathlon.R
@@ -0,0 +1,18 @@
+`pentathlon_ordertable` <-
+  structure(list(shooting = c(5L, 6L, 4L, 3L, 1L, 2L, 7L), fencing = c(1L, 
+2L, 6L, 3L, 7L, 4L, 5L), swimming = c(1L, 5L, 2L, 7L, 4L, 6L, 
+3L), riding = c(6L, 5L, 3L, 7L, 1L, 2L, 4L), running = c(5L, 
+1L, 4L, 2L, 6L, 7L, 3L)), class = "data.frame", row.names = c("Moiseev", 
+"Zadneprovskis", "Capalini", "Cerkovskis", "Meliakh", "Michalik", 
+"Walther"))
+
+`pentathlon` <-
+structure(list(brackets = list(1, c(1, 2, 3, 4, 5, 6, 7), c(1, 
+2, 3, 4, 6, 7), c(1, 2, 3, 4, 7), c(1, 2, 3, 7), c(1, 2, 4), 
+    c(1, 2, 4, 7), c(1, 2, 7), c(1, 4), 2, c(2, 3, 4, 5, 6, 7
+    ), c(2, 4, 5, 6), c(2, 4, 5, 6, 7), c(2, 4, 6), c(2, 7), 
+    3, c(3, 4, 5, 6, 7), c(3, 5), c(3, 5, 6, 7), c(3, 5, 7), 
+    4, c(4, 6), 5, 6, 7), powers = c(4, -4, -2, -2, -1, -1, -1, 
+-1, -1, 4, -2, -1, -1, -1, -1, 4, -1, -1, -1, -1, 2, -1, 3, 4, 
+3), pnames = c("Moiseev", "Zadneprovskis", "Capalini", "Cerkovskis", 
+               "Meliakh", "Michalik", "Walther")), class = "hyper2")
