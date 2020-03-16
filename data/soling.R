895247ff2f5f02d21f8c09f1d33eaac02d80dd2e
RobinHankin <hankin.robin@gmail.com>
2020-03-17 11:54:53 +1300

    new soling analysis

---
+++ data/soling.R
@@ -0,0 +1,28 @@
+`soling2000` <- 
+structure(list(I = c(5L, 3L, 1L, 4L, 2L, 8L, 13L, 12L, 9L, 14L, 
+7L, 6L, 10L, 16L, 11L, 15L), II = c(2L, 3L, 5L, 14L, 8L, 11L, 
+7L, 1L, 13L, 10L, 15L, 16L, 9L, 6L, 4L, 12L), III = c(1L, 10L, 
+13L, 2L, 4L, 6L, 8L, 7L, 11L, 3L, 5L, 12L, 9L, 14L, 15L, 16L), 
+    IV = c(3L, 2L, 11L, 16L, 8L, 1L, 4L, 6L, 9L, 7L, 10L, 12L, 
+    13L, 5L, 14L, 15L), V = c(5L, 7L, 1L, 2L, 6L, 9L, 4L, 15L, 
+    3L, 10L, 16L, 11L, 12L, 14L, 13L, 8L), VI = c(16L, 3L, 1L, 
+    2L, 9L, 14L, 13L, 12L, 7L, 11L, 5L, 4L, 8L, 10L, 15L, 6L)),
+class = "data.frame", row.names = c("norway", 
+"nz", "netherlands", "USA", "russia", "australia", "UK", "ukraine", 
+"france", "germany", "sweden", "denmark", "canada", "italy", 
+"finland", "spain"))
+
+`soling_table_2000` <-
+structure(list(I = c(5L, 3L, 1L, 4L, 2L, 8L, 13L, 12L, 9L, 14L, 
+7L, 6L, 10L, 16L, 11L, 15L), II = c(2L, 3L, 5L, 14L, 8L, 11L, 
+7L, 1L, 13L, 10L, 15L, 16L, 9L, 6L, 4L, 12L), III = c(1L, 10L, 
+13L, 2L, 4L, 6L, 8L, 7L, 11L, 3L, 5L, 12L, 9L, 14L, 15L, 16L), 
+    IV = c(3L, 2L, 11L, 16L, 8L, 1L, 4L, 6L, 9L, 7L, 10L, 12L, 
+    13L, 5L, 14L, 15L), V = c(5L, 7L, 1L, 2L, 6L, 9L, 4L, 15L, 
+    3L, 10L, 16L, 11L, 12L, 14L, 13L, 8L), VI = c(16L, 3L, 1L, 
+    2L, 9L, 14L, 13L, 12L, 7L, 11L, 5L, 4L, 8L, 10L, 15L, 6L)), 
+class = "data.frame", row.names = c("norway", 
+"nz", "netherlands", "USA", "russia", "australia", "UK", "ukraine", 
+"france", "germany", "sweden", "denmark", "canada", "italy", 
+"finland", "spain"))
+
