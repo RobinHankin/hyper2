69034b5226aa4cb45c62a5048bc3b8f0dfc2a2ec
RobinHankin <hankin.robin@gmail.com>
2020-04-03 18:07:52 +1300

    summary method for rank tables

--- R/hyper2.R
+++ R/hyper2.R
@@ -523,18 +523,37 @@ setGeneric("pnames<-",function(x,value){standardGeneric("pnames<-")})
     return(drop(out))
 }
 
-`print.ranktable` <- function(x, ...){
+`ranktable_to_printable_object` <- function(x){
   x <- rbind(x)
   if(is.null(colnames(x))){
     cn <- seq_len(ncol(x))
   } else {
     cn <- colnames(x)
   }
-  thing <- noquote(matrix(cn[x],ncol=ncol(x)))
+  thing <- matrix(cn[x],ncol=ncol(x))
   colnames(thing) <- paste("c",seq_len(ncol(x)),sep="")
   rownames(thing) <- rownames(x)
-  print(thing)
-  return(NA) # sic: this is confusing!
+  class(thing) <- "noquote"
+  return(thing)
+}
+
+`print.ranktable` <- function(x,...){
+    print(ranktable_to_printable_object(x))
+}
+
+`print.ranktablesummary` <- function(x,...){
+    x <- ranktable_to_printable_object(x)
+    class(x) <- "matrix"
+    x <- cbind(x[,1:3],"...",x[,ncol(x)+c(-1,0)])
+    x <- noquote(x)
+    print(x)
+    class(x) <- NULL
+    return(invisible(x))
+}
+
+`summary.ranktable` <- function(object, ...){
+    class(object) <- "ranktablesummary"
+    return(object)
 }
 
 `ranktable_to_ordertable` <- function(xrank){
