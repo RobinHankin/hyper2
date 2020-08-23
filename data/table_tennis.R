ab4d094e22797dda7c966f068628c3dac980a6ff
RobinHankin <hankin.robin@gmail.com>
2020-08-23 17:43:21 +1200

    new named structure

--- data/table_tennis.R
+++ data/table_tennis.R
@@ -1,4 +1,5 @@
 `table_tennis` <-
-structure(list(brackets = list(c(1, 2), c(1, 2, 3), c(1, 2, 4
-), c(1, 3), c(1, 4), 2, 3, 4), powers = c(9, -10, -8, 3, 2, 2, 
-1, 1), pnames = c("S", "a", "b", "c")), class = "hyper2")
+structure(list(brackets = list(c("S", "a"), c("S", "a", "b"), 
+    c("S", "a", "c"), c("S", "b"), c("S", "c"), "a", "b", "c"), 
+    powers = c(9, -10, -8, 3, 2, 2, 1, 1), pnames = c("S", "a", 
+    "b", "c")), class = "hyper2")
