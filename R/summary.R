01e40a5c19fea325ffe6a64898ecf8ccd0aff513
RobinHankin <hankin.robin@gmail.com>
2020-08-29 13:44:07 +1200

    tweaks

--- R/summary.R
+++ R/summary.R
@@ -1,9 +1,10 @@
 "summary.hyper2" <- function(object, ...){
     out <- list(
+        size   = size(object),
         pnames = pnames(object),
         no.of.brackets = length(brackets(object)),
-        tableoflengths = table(c(lapply(brackets(object),length),recursive=TRUE)),
         sumofpowers = sum(powers(object)),
+        tableoflengths = table(c(lapply(brackets(object),length),recursive=TRUE)),
         tableofpowers = table(powers(object))
   )
   class(out) <- "summary.hyper2"
@@ -11,13 +12,13 @@
 }
 
 "print.summary.hyper2" <- function(x, ...){
-    cat("A hyper2 object.\n")
-    cat("pnames: ", x[[1]],"\n") 
-    cat("Number of brackets:", x[[2]],"\n") 
-    cat("Sum of powers:", x[[4]],"\n") 
+    cat(paste("A hyper2 object of size ", x[[1]], ".\n",sep=""))
+    cat("pnames: ", x[[2]],"\n") 
+    cat("Number of brackets:", x[[3]],"\n") 
+    cat("Sum of powers:", x[[4]],"\n\n") 
     cat("Table of bracket lengths:")
-    print(x[[3]])
-    cat("Table of powers:\n")
     print(x[[5]])
+    cat("\nTable of powers:\n")
+    print(x[[6]])
 }
 
