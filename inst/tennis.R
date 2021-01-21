c0deea517d3ffcfa8b7ba6b57caaa1a11aafd584
RobinHankin <hankin.robin@gmail.com>
2021-01-21 16:47:39 +1300

    bugfix

--- inst/tennis.R
+++ inst/tennis.R
@@ -7,63 +7,65 @@ library(hyper2)
 ## write a throwaway function that accounts for the regular players:
 
 
-f <- function(H){
+p1 <- "p1"
+p2 <- "p2"
+p3 <- "p3"
+p4 <- "p4"
+G <- "ghost"
 
-  ## 1&3 vs 2&4, scoreline 4-4:
-  H[c(1,3)] <- H[c(1,3)] + 4
-  H[c(2,4)] <- H[c(2,4)] + 4
-  H[1:4] <- H[1:4] - 8
-  
-  ## 1&4 vs 2&3, scoreline 6-7:
-  H[c(1,4)] <- H[c(1,4)] + 6
-  H[c(2,3)] <- H[c(2,3)] + 7
-  H[1:4] <- H[1:4] - 13
+H <- hyper2()
 
-  ## 1 vs 3, scoreline 10-14:
-  H[1] <- H[1] + 10
-  H[3] <- H[2] + 14
-  H[c(1,3)] <- H[c(1,3)] - 24
+## 1&3 vs 2&4, scoreline 4-4:
+H[c(p1,p3)] %<>% inc(4)
+H[c(p2,p4)] %<>% inc(4)
+H[c(p1,p2,p3,p4)] %<>% dec(8)
 
-  ## 2 vs 3, scoreline 12-14:
-  H[2] <- H[2] + 12
-  H[3] <- H[3] + 14
-  H[c(2,3)] <- H[c(2,3)] - 26
+## 1&4 vs 2&3, scoreline 6-7:
+H[c(p1,p4)] %<>% inc(6)
+H[c(p2,p3)] %<>% inc(7)
+H[c(p1,p2,p3,p4)] %<>% dec(13)
 
-  ## 1 vs 4, scoreline 10-14:
-  H[1] <- H[1] + 10
-  H[4] <- H[4] + 14
-  H[c(1,4)] <- H[c(1,4)] - 24
+## 1 vs 3, scoreline 10-14:
+H[p1] %<>% inc(10)
+H[p3] %<>% inc(14)
+H[c(p1,p3)] %<>% dec(24)
 
-  ## 2 vs 4, scoreline 11-10:
-  H[2] <- H[2] + 11
-  H[4] <- H[4] + 10
-  H[c(2,4)] <- H[c(2,4)] - 21
+## 2 vs 3, scoreline 12-14:
+H[p2] %<>% inc(12)
+H[p3] %<>% inc(14)
+H[c(p2,p3)] %<>% dec(26)
 
-  ## 3 vs 4, scoreline 13-13:
-  H[3] <- H[3] + 13
-  H[4] <- H[4] + 13
-  H[c(3,4)] <- H[c(3,4)] - 26
-  
-  return(H)
-  }
+## 1 vs 4, scoreline 10-14:
+H[p1] %<>% inc(10)
+H[p4] %<>% inc(14)
+H[c(p1,p4)] %<>% dec(24)
 
-## First do the no-ghost case:
-H <- hyper2(d=4)
-H <- f(H)
+## 2 vs 4, scoreline 11-10:
+H[p2] %<>% inc(11)
+H[p4] %<>% inc(10)
+H[c(p2,p4)] %<>% dec(21)
+
+## 3 vs 4, scoreline 13-13:
+H[p3] %<>% inc(13)
+H[p4] %<>% inc(13)
+H[c(p3,p4)] %<>% dec(26)
+
+# First, analysis with no ghost, H_noghost:
+
+
+H_noghost <- H
 
 ## 1&2 vs 3&4, scoreline 9-2:
-H[c(1,2)] <- 9
-H[c(3,4)] <- 2
-H[1:4] <- H[1:4] - 11
-pnames(H) <- c(paste("P",1:4,sep=""))
+H_noghost[c(p1,p2)] %<>% inc(9)
+H_noghost[c(p3,p4)] %<>% inc(2)
+H_noghost[c(p1,p2,p3,p4)] %<>% dec(11)
 
 ## Now the ghost, which is player number 5:
-Hg <- hyper2(d=5)
+
+H_ghost <- H
 
 ## 1&2 vs 3&4 (NB: includes ghost!), scoreline 9-2 (again):
-Hg[c(1,2,5)] <- 9
-Hg[c(3,4)] <- 2
-Hg[1:5] <- Hg[1:5] - 11
+H_ghost[c(p1,p2,G)] %<>% inc(9)
+H_ghost[c(p3,p4)] %<>% inc(2)
+H_ghost[c(p1,p2,p3,p4,G)] %<>% dec(11)
 
-Hg <- f(Hg)
-pnames(Hg) <- c(paste("P",1:4,sep=""),"ghost")
