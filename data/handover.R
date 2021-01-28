83d704c253a09ba3f544c6e0efab019dde8df784
RobinHankin <hankin.robin@gmail.com>
2021-01-28 16:40:52 +1300

    consistentification of handover work

--- data/handover.R
+++
@@ -1,5 +0,0 @@
-`handover` <-
-structure(list(brackets = list("t00", c("t00", "t01"), c("t00", 
-"t10"), "t01", c("t01", "t11"), "t10", c("t10", "t11"), "t11"), 
-    powers = c(18, 9, 4, 5, 4, 1, 2, 26), pnames = c("t00", "t01", 
-    "t10", "t11")), class = "hyper2")
