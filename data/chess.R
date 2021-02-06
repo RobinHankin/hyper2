163d3f764118888fc85546abf19a374960a6e0d7
Robin Hankin <hankin.robin@gmail.com>
2021-02-06 20:56:36 +1300

    consistentify chess

--- data/chess.R
+++
@@ -1,7 +0,0 @@
-`chess` <- 
-  structure(list(
-      brackets = list("Topalov",
-                      c("Topalov", "Anand"), c("Topalov", "Karpov"), "Anand", c("Anand", "Karpov"), "Karpov"),
-      powers = c(30, -35, -18, 36, -35, 22),
-      pnames   = c("Topalov", "Anand", "Karpov")),
-      class = "hyper2")
