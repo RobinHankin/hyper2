330c0c777bd7964117c4408b9e58d95fe8581cea
Robin Hankin <hankin.robin@gmail.com>
2021-02-07 14:10:37 +1300

    consistentify table tennis

--- data/table_tennis.R
+++
@@ -1,5 +0,0 @@
-`table_tennis` <-
-structure(list(brackets = list(c("S", "a"), c("S", "a", "b"), 
-    c("S", "a", "c"), c("S", "b"), c("S", "c"), "a", "b", "c"), 
-    powers = c(9, -10, -8, 3, 2, 2, 1, 1), pnames = c("S", "a", 
-    "b", "c")), class = "hyper2")
