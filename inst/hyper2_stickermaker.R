db02ba39c27251b7222727981b5365f8d1c5a870
Robin Hankin <hankin.robin@gmail.com>
2021-09-20 20:52:29 +1200

    correct colors

--- inst/hyper2_stickermaker.R
+++ inst/hyper2_stickermaker.R
@@ -1,7 +1,4 @@
 library("hexSticker")
-library("hyper2")
-
-imgurl <- system.file("monster_blue.png", package="hyper2")
-sticker(imgurl, package="hyper2", p_size=8, s_x=1, s_y=0.84,
-s_width=0.7,asp=0.85, white_around_sticker=TRUE, h_fill="#7733FF",
+sticker("monster_blue.png", package="hyper2", p_size=24, s_x=1, s_y=0.84,
+s_width=0.7,asp=sqrt(3)/2, white_around_sticker=TRUE, h_fill="#7733FF",
 h_color="#000000", filename="hyper2.png")
