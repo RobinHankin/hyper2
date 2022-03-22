ca77321c31ba0cadd1765dfb1d390bdda7130596
Robin Hankin <hankin.robin@gmail.com>
2022-03-22 13:58:52 +1300

    issue #121

--- R/metropolis_hastings.R
+++ R/metropolis_hastings.R
@@ -1,41 +1,16 @@
-## library("EMC")
-
 `rp` <- function(n, H, startp=NULL, fcm=NULL, fcv=NULL,SMALL=1e-6, l=loglik,...){
 
-  myfunc <- function(ignore){
-    logTarDensFunc <- function(draw,...) {
+  mypi <- function(p){
       s <- size(H)
       UI <- rbind(diag(nrow = s - 1), -1, fcm)
       CI <- c(rep(SMALL, s - 1), -1 + SMALL, fcv)
-      if(any(UI %*% draw < CI)){
-        return(-Inf)
+      if(any(UI %*% p < CI)){
+        return(0)
       } else {
-         return(l(draw,H))
+         return(l(p,H,log=FALSE))
       }
     }
-    
-    proposalSD <- rep(0.01,size(H)-1)
-    propNewFunc <- function(block, currentDraw, ...) {
-      proposalDraw <- currentDraw
-      proposalDraw[block] <- rnorm(1, currentDraw[block], proposalSD[block])
-      proposalDraw
-    }
-    list(logTarDensFunc = logTarDensFunc, propNewFunc = propNewFunc)
-  }
+    if(is.null(startp)){startp <- indep(equalp(H))}
 
-  if(is.null(startp)){startp <- indep(maxp(H))}
-  out <-
-    with(myfunc(),
-         EMC::randomWalkMetropolis(nIters         = n,
-                                   startingVal    = startp,
-                                   logTarDensFunc = logTarDensFunc,
-                                   propNewFunc    = propNewFunc,
-                                   verboseLevel   = 0))
-    
-  out <- out$draws
-  out <- cbind(out,1-rowSums(out))
-  if(!identical(pnames(H),NA)){
-      colnames(out) <- pnames(H)
-  }
-  return(out)
+    calibrator::MH(n,start=startp,sigma=diag(0.01,nrow=size(H)-1),pi=mypi)
 }
