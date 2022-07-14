cd9c8d2003cfb4ccdd541d05cac33eefd2ce9698
Robin Hankin <hankin.robin@gmail.com>
2022-07-14 15:17:13 +1200

    fixes #146

--- R/cheering.R
+++ R/cheering.R
@@ -1,17 +1,26 @@
 `cheering3` <- function(v,e,help){
 
-  ## competitors abcdef, with two groups of mutual friends: (abc) with
-  ## mutual helpfulness 1.1, and (de) with mutual helpfulness 1.3:
+  ## competitors abcdef, with two groups of mutual friends: (abd) with
+  ## mutual helpfulness 1.1, and (cf) with mutual helpfulness 1.3;
+  ## competitors e and g are in groups of 1.
 
   ## use-case:
-  ## cheering3(c("a","b","c","d","e","f"),c(a=1,b=1,c=1,d=2,e=2,f=3),help=c(1.1,1.3))
-
-  
+  ## cheering3(v=letters[1:7],e=c(a=1,b=1,c=2,d=1,f=2),help=c(1.88,1.66))
 
 
   stopifnot(all(table(names(e))==1))
-  stopifnot(all(sort(names(e)) == sort(unique(v))))
-    
+  stopifnot(all(e>0))
+  stopifnot(all(e==round(e)))
+  stopifnot(all(table(v)==1))
+  stopifnot(all(names(e) %in% v)) 
+
+  no_group <- !(v %in% names(e))
+  if(any(no_group)){
+    jj <- seq(from=max(e)+1,len=sum(no_group))
+    names(jj) <- v[no_group]   # a character vector
+    e <- c(e,jj)
+  }
+
   out <- hyper3()
 
   finished <- NULL # these are competitors who have finished and are cheering their friends
@@ -24,6 +33,7 @@
 
     winner_ec_number <- e[names(e)==winner]  # this would be an integer; "ec" means "equivalence class"
     finished_ec_number <- e[names(e) %in% finished]
+
     if(winner_ec_number %in% finished_ec_number){  # that is, if the winner has a friend who has finished...
       jjn[] <- help[winner_ec_number]   # ...then he gets extra strength.
     }
@@ -34,7 +44,6 @@
       runner_ec_number <- e[names(e) == still_running[i]]
       if(length(finished_ec_number)>0){
         if(length(runner_ec_number) ==0){browser()}
-        out
         if(runner_ec_number %in% finished_ec_number){
           jjd[i] <- help[runner_ec_number]
         }
