8ae1e59771ef8724b466932d33c0ac05d9b5ec5b
Robin Hankin <hankin.robin@gmail.com>
2023-02-10 10:04:46 +1300

    summary method for hyper3 objects

--- R/summary.R
+++ R/summary.R
@@ -18,7 +18,29 @@
     cat("Sum of powers:", x[[4]],"\n\n") 
     cat("Table of bracket lengths:")
     print(x[[5]])
-    cat("\nTable of powers:\n")
+    cat("\nTable of powers:")
     print(x[[6]])
+}
+
+"summary.hyper3" <- function(object, ...){
+  out <- c(
+      summary.hyper2(object), 
+      list(tableofweights = table(unlist(weights(object))))
+  )
+  class(out) <- "summary.hyper3"
+  return(out)
+}
+
+"print.summary.hyper3" <- function(x, ...){
+    cat(paste("A hyper3 object of size ", x[[1]], ".\n",sep=""))
+    cat("pnames: ", x[[2]],"\n") 
+    cat("Number of brackets:", x[[3]],"\n") 
+    cat("Sum of powers:", x[[4]],"\n\n") 
+    cat("Table of bracket lengths:")
+    print(x[[5]])
+    cat("\nTable of powers:")
+    print(x[[6]])
+    cat("\nTable of weights:")
+    print(x[[7]])
 }
 
