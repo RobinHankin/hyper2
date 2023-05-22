2673438e20370c8bceb9e90c560c42ed735fbe38
Robin Hankin <hankin.robin@gmail.com>
2023-05-22 15:49:15 +1200

    test suite kickoff

---
+++ tests/testthat/test_aaa.R
@@ -0,0 +1,11 @@
+# modelled on mvp/tests/testthat/test_aad.R which tests specific
+# identities.
+
+test_that("Test suite,  specific identities and miscellaneous checks",{
+
+  x <- rhyper2()
+  expect_true(x==x)
+
+
+
+})
