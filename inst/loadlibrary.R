84d96c0420b698359189ecd21b6387de6f50d946
RobinHankin <hankin.robin@gmail.com>
2025-02-18 09:04:03 +0000

    cheering (tikz diagram is bad atm)

--- inst/loadlibrary.R
+++ inst/loadlibrary.R
@@ -1,3 +1,4 @@
 suppressMessages(library("knitr",quietly=TRUE,verbose=FALSE))
 suppressMessages(library("hyper2",quietly=TRUE,verbose=FALSE))
 suppressMessages(library("magrittr",quietly=TRUE,verbose=FALSE))
+suppressMessages(library("tikzDevice",quietly=TRUE,verbose=FALSE))
