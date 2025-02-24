627065476b90bafab72fc82262d43d85d5e02b30
RobinHankin <hankin.robin@gmail.com>
2025-02-24 08:25:30 +0000

    more to ignore

---
+++ R/suppfun.R
@@ -0,0 +1,6 @@
+setGeneric("suppfun",function(x){standardGeneric("suppfun")})
+
+`suppfun` <- function(M){UseMethod("suppfun")}
+`suppfun.ordertable` <- function(x){ordertable2supp(x)}
+`suppfun.ranktable` <- function(x){"not yet implemented"}
+`suppfun.preftable` <- function(x){"not yet implemented"}
