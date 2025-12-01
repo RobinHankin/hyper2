dbebc3a3a2e9285e51c893bf4d7846edcc512f0d
RobinHankin <hankin.robin@gmail.com>
2025-12-01 15:00:33 +0000

    further tests

--- tests/testthat/test_ranktable.R
+++ tests/testthat/test_ranktable.R
@@ -4,5 +4,16 @@ test_that("ranktable tests", {
     expect_output(print(x))
     expect_output(print(summary(x)))
 
+    expect_error(ranktable_to_ordertable(x))
+
+    expect_silent(ordertable_to_ranktable(pentathlon_table))
+
+    x <- cbind(c(a=3, b=1, c=2), 1:3)
+    colnames(x) <- c("j1", "j2")
+    x <-  as.ordertable(x)
+    y <- as.ranktable(x)
+    expect_true(all(unclass(y) == letters[c(2,1,3,2,1,3)]))
+
+    expect_true(suppfun(pentathlon_table) == pentathlon)
     
 } )
