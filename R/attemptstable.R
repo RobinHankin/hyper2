6727f06f0dfa2e67970863c3ad2157709c1fedfe
RobinHankin <hankin.robin@gmail.com>
2026-05-26 08:27:52 +0100

    fixes #264

--- R/attemptstable.R
+++ R/attemptstable.R
@@ -46,12 +46,12 @@ as.vector.attemptstable <- function(x, mode){
     attemptstable2supp3(x, decreasing=TRUE, give.supp=FALSE, dnf.last=TRUE)
 }
 
-`attemptstable2supp3` <- function(x, decreasing=TRUE, give.supp=TRUE, dnf.last=TRUE){
+`attemptstable2supp3` <- function(x, decreasing=TRUE, give.supp=TRUE, nothrow_loses=FALSE){
     o <- c(matrix(suppressWarnings(as.numeric(c(x, recursive=TRUE))), nrow(x), ncol(x)))
     names(o) <- c(matrix(rownames(x), nrow(x), ncol(x)))
     o <- sort(o, na.last=TRUE, decreasing=decreasing)
     if(give.supp){
-        if(dnf.last){
+        if(nothrow_loses){
             nf <- names(o)[is.na(o)]
         } else {
             nf <- NULL
